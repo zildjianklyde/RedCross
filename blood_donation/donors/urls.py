ad9e27e0f5138070cc2895fd27aa4e63e7c7859a
zildjianklyde <zildjianklyde@gmail.com>
2025-01-03 02:07:49 +0800

    asd

--- blood_donation/donors/urls.py
+++ blood_donation/donors/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from .views import DonorCreateView, DonorListView, DonorDetailView, DonorUpdateView, DonorDeleteView
+from django.contrib.auth.views import LoginView, LogoutView  # Correct import for LogoutView
+from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
     path('', DonorListView.as_view(), name='donor_list'),
     path('add/', DonorCreateView.as_view(), name='donor_add'),
     path('<int:pk>/', DonorDetailView.as_view(), name='donor_detail'),
     path('<int:pk>/edit/', DonorUpdateView.as_view(), name='donor_edit'),
     path('<int:pk>/delete/', DonorDeleteView.as_view(), name='donor_delete'),
+    path('', views.landing_page, name='landing_page'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='donors/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='donors/logout.html'), name='logout'),  # Use the built-in LogoutView
 ]
--- blood_donation/donors/views.py
+++ blood_donation/donors/views.py
@@ -2,6 +2,28 @@ from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Donor
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = 'donors/home.html'
+    
+    
+def landing_page(request):
+    return render(request, 'donors/landing_page.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'donors/register.html', {'form': form})
 
 class DonorListView(ListView):
     model = Donor
@@ -27,3 +49,5 @@ class DonorDeleteView(DeleteView):
     model = Donor
     template_name = 'donors/donor_confirm_delete.html'
     success_url = reverse_lazy('donor_list')
+class LoginView(LoginView):
+    template_name = 'donors/login.html'
