6d6b027607aacbe7e5233c17aed342d47a5d8382
zildjianklyde <zildjianklyde@gmail.com>
2025-01-03 03:19:35 +0800

    ras

--- blood_donation/donors/urls.py
+++ blood_donation/donors/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import DonorCreateView, DonorListView, DonorDetailView, DonorUpdateView, DonorDeleteView
-from django.contrib.auth.views import LoginView, LogoutView  # Correct import for LogoutView
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
@@ -12,6 +12,6 @@ urlpatterns = [
     path('<int:pk>/delete/', DonorDeleteView.as_view(), name='donor_delete'),
     path('', views.landing_page, name='landing_page'),
     path('register/', views.register, name='register'),
-    path('login/', LoginView.as_view(template_name='donors/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='donors/logout.html'), name='logout'),  # Use the built-in LogoutView
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
