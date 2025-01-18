b50b3699478a558fdb5563668c4c24ef29d06296
zildjianklyde <150168399+zildjianklyde@users.noreply.github.com>
2025-01-18 16:22:12 +0800

    Delete donation directory

--- donation/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser, Eligibility, Donation
-
-admin.site.register(CustomUser)
-admin.site.register(Eligibility)
-admin.site.register(Donation)
